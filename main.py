891592448db1ad6bc7359f2da7529e966a7ec839
Sree Abiraami T <sreeabiraami23@gmail.com>
2026-06-25 11:45:45 +0530

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
@@ -17,10 +19,7 @@ app = FastAPI(
 # This prevents browsers from blocking your frontend requests (CORS errors)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "https://eb-billanalyzer.netlify.app",
-    "http://localhost:5173"
-],
+    allow_origins=["https://eb-billanalyzer.netlify.app", "http://localhost:5173"],  # your actual frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -54,9 +53,11 @@ def home():
     Root landing response message.
     """
     return "Electricity bill analyser"
+
+
 if __name__ == "__main__":
     # Read the dynamic PORT variable given by Render, default to 8000 locally
     port = int(os.environ.get("PORT", 8000))
-    
+
     # CRUCIAL: Must bind to host "0.0.0.0" so Render can find it
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
