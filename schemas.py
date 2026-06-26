2041025dfb0fd77f5e7cbcbc076ceff1368b9273
Sree Abiraami T <sreeabiraami23@gmail.com>
2026-06-26 20:58:46 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict,Field
 from datetime import date, datetime
 from typing import List
 
 class BillOut(BaseModel):
     id: int
-    user_id: int
     consumer_number: str
+    region:str
     bill_month: date
     tariff_category: str
     units_consumed: float
@@ -22,19 +22,20 @@ class AnalysisOut(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
 class UploadBillResponse(BaseModel):
-    bill_id: int
+    consumer_number: str
     tariff_category: str
+    region:str
     units_consumed: float
     amount_due: float
     bill_month: str
     ai_summary: str
-    saving_tips: List[str]  # <-- Add this to match what the pipeline unifies!
+    saving_tips: List[str]
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class TipsResponse(BaseModel):
-    user_id: int
+    consumer_number: str
     saving_tips: List[str]
 
 
@@ -45,7 +46,7 @@ class HistoryItem(BaseModel):
 
 
 class UserOut(BaseModel):
-    id: int
+    consumer_number:str
     name: str
     email: str
     created_at: datetime
@@ -53,7 +54,7 @@ class UserOut(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
 class BillCreate(BaseModel):
-    consumer_number: str
+    consumer_number: str=Field(...,min_length=10,max_length=12)
     bill_month: date
     tariff_category: str
     units_consumed: float
