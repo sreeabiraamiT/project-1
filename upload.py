a1cfda474b186ffc6f4ddd8e8b5cb79ac7ef8a07
Sree Abiraami T <sreeabiraami23@gmail.com>
2026-06-27 20:33:49 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -2,11 +2,10 @@ from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form
 from sqlalchemy.orm import Session
 from database import get_db
 import models
-from schemas import UploadBillResponse
+from schemas import UploadBillResponse,CostBreakdown
 from datetime import date
 from typing import Optional
 
-# Import the unified Student 2 + Student 3 processing pipeline
 from services import (
     run_complete_student2_pipeline,
     generate_tips_for_manual_entry,
@@ -29,38 +28,137 @@ REGION_MAP = {
 
 def calculate_amount_due(units_consumed: float, tariff_category: str):
     tariff = tariff_category.strip().lower()
-
+    cost_breakdown=[]
     if tariff == "domestic":
+        cost_breakdown = []
+
         if units_consumed <= 200:
             amount_due = 0
+
+            cost_breakdown.append({
+            "slab": "0-200",
+            "units": units_consumed,
+            "rate": 0,
+            "cost": 0
+        })
+
         elif units_consumed <= 400:
-            amount_due = (units_consumed - 200) * 4.70
+            slab2 = units_consumed - 200
+            amount_due = slab2 * 4.70
+
+            cost_breakdown = [
+            {"slab": "0-200", "units": 200, "rate": 0, "cost": 0},
+            {"slab": "201-400", "units": slab2, "rate": 4.70, "cost": round(slab2 * 4.70, 2)}
+        ]
+
         elif units_consumed <= 500:
-            amount_due = (200 * 4.70) + ((units_consumed - 400) * 6.30)
+            slab2 = 200
+            slab3 = units_consumed - 400
+
+            amount_due = (slab2 * 4.70) + (slab3 * 6.30)
+
+            cost_breakdown = [
+            {"slab": "0-200", "units": 200, "rate": 0, "cost": 0},
+            {"slab": "201-400", "units": 200, "rate": 4.70, "cost": 940},
+            {"slab": "401-500", "units": slab3, "rate": 6.30, "cost": round(slab3 * 6.30, 2)}
+        ]
+
         elif units_consumed <= 600:
-            amount_due = (200 * 4.70) + (100 * 6.30) + ((units_consumed - 500) * 8.40)
+            slab2 = 200
+            slab3 = 100
+            slab4 = units_consumed - 500
+
+            amount_due = (slab2 * 4.70) + (slab3 * 6.30) + (slab4 * 8.40)
+
+            cost_breakdown = [
+            {"slab": "0-200", "units": 200, "rate": 0, "cost": 0},
+            {"slab": "201-400", "units": 200, "rate": 4.70, "cost": 940},
+            {"slab": "401-500", "units": 100, "rate": 6.30, "cost": 630},
+            {"slab": "501-600", "units": slab4, "rate": 8.40, "cost": round(slab4 * 8.40, 2)}
+        ]
+
         elif units_consumed <= 800:
-            amount_due = (200 * 4.70) + (100 * 6.30) + (100 * 8.40) + ((units_consumed - 600) * 9.45)
+            slab2 = 200
+            slab3 = 100
+            slab4 = 100
+            slab5 = units_consumed - 600
+
+            amount_due = (
+            (slab2 * 4.70)
+            + (slab3 * 6.30)
+            + (slab4 * 8.40)
+            + (slab5 * 9.45)
+        )
+
+            cost_breakdown = [
+            {"slab": "0-200", "units": 200, "rate": 0, "cost": 0},
+            {"slab": "201-400", "units": 200, "rate": 4.70, "cost": 940},
+            {"slab": "401-500", "units": 100, "rate": 6.30, "cost": 630},
+            {"slab": "501-600", "units": 100, "rate": 8.40, "cost": 840},
+            {"slab": "601-800", "units": slab5, "rate": 9.45, "cost": round(slab5 * 9.45, 2)}
+        ]
+
         elif units_consumed <= 1000:
+            slab2 = 200
+            slab3 = 100
+            slab4 = 100
+            slab5 = 200
+            slab6 = units_consumed - 800
+
             amount_due = (
-                (200 * 4.70)
-                + (100 * 6.30)
-                + (100 * 8.40)
-                + (200 * 9.45)
-                + ((units_consumed - 800) * 10.50)
-            )
+            (slab2 * 4.70)
+            + (slab3 * 6.30)
+            + (slab4 * 8.40)
+            + (slab5 * 9.45)
+            + (slab6 * 10.50)
+        )
+
+            cost_breakdown = [
+            {"slab": "0-200", "units": 200, "rate": 0, "cost": 0},
+            {"slab": "201-400", "units": 200, "rate": 4.70, "cost": 940},
+            {"slab": "401-500", "units": 100, "rate": 6.30, "cost": 630},
+            {"slab": "501-600", "units": 100, "rate": 8.40, "cost": 840},
+            {"slab": "601-800", "units": 200, "rate": 9.45, "cost": 1890},
+            {"slab": "801-1000", "units": slab6, "rate": 10.50, "cost": round(slab6 * 10.50, 2)}
+        ]
+
         else:
+            slab2 = 200
+            slab3 = 100
+            slab4 = 100
+            slab5 = 200
+            slab6 = 200
+            slab7 = units_consumed - 1000
+
             amount_due = (
-                (200 * 4.70)
-                + (100 * 6.30)
-                + (100 * 8.40)
-                + (200 * 9.45)
-                + (200 * 10.50)
-                + ((units_consumed - 1000) * 11.55)
-            )
+            (slab2 * 4.70)
+            + (slab3 * 6.30)
+            + (slab4 * 8.40)
+            + (slab5 * 9.45)
+            + (slab6 * 10.50)
+            + (slab7 * 11.55)
+        )
+
+            cost_breakdown = [
+            {"slab": "0-200", "units": 200, "rate": 0, "cost": 0},
+            {"slab": "201-400", "units": 200, "rate": 4.70, "cost": 940},
+            {"slab": "401-500", "units": 100, "rate": 6.30, "cost": 630},
+            {"slab": "501-600", "units": 100, "rate": 8.40, "cost": 840},
+            {"slab": "601-800", "units": 200, "rate": 9.45, "cost": 1890},
+            {"slab": "801-1000", "units": 200, "rate": 10.50, "cost": 2100},
+            {"slab": "Above 1000", "units": slab7, "rate": 11.55, "cost": round(slab7 * 11.55, 2)}
+        ]
 
     elif tariff == "commercial":
         amount_due = units_consumed * 8.0
+        cost_breakdown = [
+        {
+            "slab": "Commercial",
+            "units": units_consumed,
+            "rate": 8.00,
+            "cost": round(amount_due, 2)
+        }
+    ]
 
     else:
         raise HTTPException(
@@ -68,7 +166,10 @@ def calculate_amount_due(units_consumed: float, tariff_category: str):
             detail="Invalid tariff category."
         )
 
-    return round(amount_due, 2)
+    return {
+    "amount_due": round(amount_due, 2),
+    "cost_breakdown": cost_breakdown
+}
     
 
 @router.post("/upload-bill", response_model=UploadBillResponse)
@@ -119,7 +220,9 @@ async def upload_bill(
         if len(consumer_number) not in (10, 12):
             raise HTTPException(status_code=400, detail="Consumer number must be 10 or 12 digits.")
         region_code=consumer_number[:2]
-        region_name=REGION_MAP.get(region_code,"UNKNOWN")
+        region_name=REGION_MAP.get(region_code)
+        if region_name is None:
+            raise HTTPException(status_code=400,detail="Invalid consumer number. Unknown region code.")
         # Look up or create the User mapped to this consumer number
         user = db.query(models.User).filter(models.User.consumer_number == consumer_number).first()
         
@@ -135,12 +238,14 @@ async def upload_bill(
             except Exception:
                 db.rollback()
                 # Safe fallback if concurrent requests attempt creation simultaneously
-                user = db.query(models.User).filter(models.User.name == user_name).first()
+                user = db.query(models.User).filter(models.User.consumer_number == consumer_number).first()
     
-        amount_due = calculate_amount_due(units_consumed=units_consumed, tariff_category=tariff_category)
+        bill = calculate_amount_due(units_consumed=units_consumed, tariff_category=tariff_category)
+        amount_due=bill["amount_due"]
+        cost_breakdown=bill["cost_breakdown"]
         
         # Save bill records to the database
-        new_bill = models.Bill(consumer_number=user.consumer_number,region=region_name,bill_month=bill_month,tariff_category=tariff_category,units_consumed=units_consumed,amount_due=amount_due)
+        new_bill = models.Bill(consumer_number=user.consumer_number,region=region_name,bill_month=bill_month,tariff_category=tariff_category,units_consumed=units_consumed,amount_due=amount_due,cost_breakdown=cost_breakdown)
         db.add(new_bill)
         db.commit()
         db.refresh(new_bill)
@@ -163,7 +268,8 @@ async def upload_bill(
     amount_due=new_bill.amount_due,
     bill_month=str(new_bill.bill_month),
     ai_summary=new_analysis.ai_summary,
-    saving_tips=new_analysis.saving_tips
+    saving_tips=new_analysis.saving_tips,
+    cost_breakdown=new_bill.cost_breakdown
 )
 
     except HTTPException:
